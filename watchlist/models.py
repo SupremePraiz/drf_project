fb070b950cb5ef9cb7b9da7b7302a355c25baab6
Jude Adeyemi <supremepraiz2@gmail.com>
2025-03-20 21:21:05 +0100

    added content to the model

--- watchlist/models.py
+++ watchlist/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
 # Create your models here.
+class StreamPlatform(models.Model):
+    name = models.CharField(max_length=30)
+    about = models.CharField(max_length=150)
+    website = models.URLField(max_length=100)
+
+    def __str__(self):
+        return self.name
+    
+class WatchList(models.Model):
+    title = models.CharField(max_length=50)
+    story_line = models.CharField(max_length=200)
+    platform = models.ForeignKey(StreamPlatform, on_delete=models.CASCADE, related_name='watchlist')
+    is_active = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+    
+class Review(models.Model):
+    rating = models.FloatField()
+    description = models.CharField(max_length=200, null=True)
+    watchlist = models.ForeignKey(WatchList, on_delete=models.CASCADE, related_name='reviews')
+    is_active = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+
+    def __str__(self):
+        return str(self.rating)
